2c16636c80d036bfdd791ffe74f1b727df38b5db
saisrivathsa <srivathsaeric@gmail.com>
2017-02-03 22:24:26 +0530

    Added new user create function

--- auth_model/urls.py
+++ auth_model/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^auth_view/$', views.auth_view, name='auth_view'),
     url(r'^success/$', views.success, name='success'),
     url(r'^logout/$', views.logout, name='logout'),
+    url(r'^create_new_user/$', views.create_new_user, name='create_new_user'),
+    url(r'^sign_up/$', views.sign_up, name='sign_up'),
 ]
--- auth_model/views.py
+++ auth_model/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 #for authentication uses
 from django.contrib import auth
+# for new user
+from django.contrib.auth.models import User
 
 
 def login(request) :
@@ -26,3 +28,21 @@ def auth_view(request) :
     else :
         return render(request, "auth_model/login.html", {'error':'Invalid Credentials'})
         #return HttpResponseRedirect(reverse("auth_model:login", kwargs={'error' : 'Invalid Credentials
+
+def sign_up(request):
+    return render(request, 'auth_model/create_new_user.html')
+
+def create_new_user(request):
+    try:
+        user = User.objects.create_user(
+        username = request.POST['username'],
+        password = request.POST['password'],
+        email = request.POST['email'],
+        )
+        user.save()
+        username = request.POST.get('username', '' )
+        password = request.POST.get('password', '' )
+        user = auth.authenticate( username=username, password=password )
+        return HttpResponse('<h2>Created new user %s </h2><br><a href="../signup/">  create another account?</a>' %user.username)
+    except :
+        return render( request, 'auth_model/create_new_user.html', {'error':'Error making account. Try again'} )
